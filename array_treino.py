0389de7911c5ea682df23d50b1cd3b6ac913f454
Francisco Willians <franciscowillians@outlook.com>
2026-02-01 15:01:57 -0300

    teste de array e  dtype

---
+++ array_treino.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+teste1 = np.array([3.14, 4, 2, 3]) #só aceita um  tipo de dado se diferente ele mudar todos para unificar
+
+
+#dtype altera o tipo dos dados presente na array
+teste2 = np.array([1, 2, 3, 4], dtype='float32') #np 
+print(teste1)
+print(teste2)
